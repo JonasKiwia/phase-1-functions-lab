b4885c367ee0ae4c32ff3c93d210b6bf73db5dbc
JonasKiwia <jonas.kiwia@student.moringaschool.com>
2024-11-27 12:39:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  const headquarters = 42; // Scuber's HQ is at block 42
+  return Math.abs(someValue - headquarters); // Absolute value for distance
+}
+
+
+function distanceFromHqInFeet(someValue) {
+  const blocks = distanceFromHqInBlocks(someValue); // Get distance in blocks
+  return blocks * 264; // Convert blocks to feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+  const blocksTravelled = Math.abs(start - destination); // Absolute block distance
+  return blocksTravelled * 264; // Convert blocks to feet
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination); // Get distance in feet
+
+  if (distance <= 400) {
+    return 0; // First 400 feet are free
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02; // 2 cents per foot after the first 400 feet
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25; // Flat fare for distances over 2000 feet and under 2500 feet
+  } else {
+    return 'cannot travel that far'; // Trips over 2500 feet are not allowed
+  }
+}
